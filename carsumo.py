0847001968f5a85f5afae872dc5a8baa8f42bcf3
rasp <rinrin.int@proton.me>
2024-07-09 20:20:14 +0700

    simple sumo algo cleaned

--- carsumo.py
+++ carsumo.py
@@ -1,13 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-#from gpiozero import DistanceSensor
-#packages doesn't work well with RPi.GPIO
-
-#ultrasonic = DistanceSensor(echo=17,trigger=3)
-#ultrasonic.distance for distance
-#to do, change the ir to be used for edge detection, 3 points detect white = walk straight
-
 
 GPIO_ECHO = 17
 GPIO_TRIGGER = 3
@@ -38,28 +31,22 @@ g.start(100)
 def distance():
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
- 
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
     StartTime = time.time()
     StopTime = time.time()
- 
     # save StartTime
     while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
- 
     # save time of arrival
     while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
- 
     # time difference between start and arrival
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
     distance = (TimeElapsed * 34300) / 2
- 
     return distance
 
 
@@ -100,24 +87,6 @@ try:
         else:
             turn_left()
             print('spin')
-        '''
-        if GPIO.input(l) and GPIO.input(c) and GPIO.input(r):
-            GPIO.output([in1,in2,in3,in4],GPIO.LOW)
-            print("ended")
-            break
-        elif GPIO.input(c):
-            print("center")
-            move_forward()
-        elif GPIO.input(r):
-            print("right")
-            set_speed(0,100)
-            turn_right()
-        elif GPIO.input(l):
-            print("left")
-            set_speed(100,0)
-            turn_left()
-        #sleep(.1)
-        '''
     GPIO.cleanup()
 except KeyboardInterrupt:
     GPIO.output([in1,in2,in3,in4],GPIO.LOW)
